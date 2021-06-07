b73948cfa1ec09bc3adc91853eac7765c5c3bb16
Peter Prince <github@pcprince.co.uk>
2021-06-07 16:50:25 +0100

    Lint and reformat

--- wav-spectrogram.js
+++ wav-spectrogram.js
@@ -6,13 +6,13 @@
 
 'use strict';
 
-/*jslint plusplus: true */
+/* jslint plusplus: true */
 
-var dsp = require('dsp.js-browser');
-var decode = require('audio-decode');
-var colormap = require('colormap');
+const dsp = require('dsp.js-browser');
+const decode = require('audio-decode');
+const colormap = require('colormap');
 
-function scaleAcrossRange(x, max, min) {
+function scaleAcrossRange (x, max, min) {
 
     return (x - min) / (max - min);
 
@@ -20,9 +20,13 @@ function scaleAcrossRange(x, max, min) {
 
 function median(values) {
 
-    values.sort(function (a, b) {return a - b; });
+    values.sort((a, b) => {
 
-    var half = Math.floor(values.length / 2);
+        return a - b;
+
+    });
+
+    const half = Math.floor(values.length / 2);
 
     if (values.length % 2) {
 
@@ -36,17 +40,15 @@ function median(values) {
 
 function medianFilter(array) {
 
-    var i, j, values, filteredArray, filteredRow;
+    const filteredArray = [];
 
-    filteredArray = [];
+    for (let i = 1; i < array.length - 1; i++) {
 
-    for (i = 1; i < array.length - 1; i++) {
+        const filteredRow = [];
 
-        filteredRow = [];
+        for (let j = 1; j < array[i].length - 1; j++) {
 
-        for (j = 1; j < array[i].length - 1; j++) {
-
-            values = [];
+            const values = [];
             values.push(array[i - 1][j], array[i][j], array[i + 1][j]);
             values.push(array[i - 1][j - 1], array[i][j - 1], array[i + 1][j - 1]);
             values.push(array[i - 1][j + 1], array[i][j + 1], array[i + 1][j + 1]);
@@ -65,51 +67,49 @@ function medianFilter(array) {
 
 function drawSpectrogram(params, callback) {
 
-    var arrayBuffer, canvasElem, cmap, nfft, frameLengthMs, frameStepMs, err, sampleRate, samples, sampleArray, frameLength, frameStep, numFrames, paddedArrayLength, frames, i, maxValue, minValue, spectrumFrames, spectrum, m, n, a, o, p, ctx, specWidth, specHeight, colours;
-
-    arrayBuffer = params.arrayBuffer;
-    canvasElem = params.canvasElem;
-    cmap = params.cmap;
-    nfft = params.nfft || 512;
-    frameLengthMs = params.frameLengthMs || 0.1;
-    frameStepMs = params.frameStepMs || 0.005;
+    const arrayBuffer = params.arrayBuffer;
+    const canvasElem = params.canvasElem;
+    const cmap = params.cmap;
+    const nfft = params.nfft || 512;
+    const frameLengthMs = params.frameLengthMs || 0.1;
+    const frameStepMs = params.frameStepMs || 0.005;
 
     decode(arrayBuffer, (err, audioBuffer) => {
 
-        if(err || audioBuffer.length === 0) {
-            console.error("Loading file failed!");
+        if (err || audioBuffer.length === 0) {
+
+            console.error('Failed to load file');
             typeof params.errorHandler === 'function' && params.errorHandler();
             return;
+
         }
 
         // Extract samples from audio file
-        sampleRate = audioBuffer.sampleRate;
-        samples = audioBuffer.getChannelData(0);
-        sampleArray = Array.prototype.slice.call(samples);
+        const sampleRate = audioBuffer.sampleRate;
+        const samples = audioBuffer.getChannelData(0);
+        let sampleArray = Array.prototype.slice.call(samples);
 
-        frameLength = frameLengthMs * sampleRate;
-        frameStep = frameStepMs * sampleRate;
+        const frameLength = frameLengthMs * sampleRate;
+        const frameStep = frameStepMs * sampleRate;
 
         // Pad signal to make sure that all frames have equal number of samples without truncating any samples from the original signal
-        numFrames = Math.ceil((samples.length - frameLength) / frameStep);
-        paddedArrayLength = numFrames * frameStep + frameLength;
+        const numFrames = Math.ceil((samples.length - frameLength) / frameStep);
+        const paddedArrayLength = numFrames * frameStep + frameLength;
         sampleArray = sampleArray.concat(new Array(paddedArrayLength - samples.length).fill(0));
 
-        frames = [];
+        const frames = [];
 
-        for (i = 0; i < numFrames; i++) {
+        for (let i = 0; i < numFrames; i++) {
 
-            let frameStart, frame, j, frameIndex, filteredSample;
+            const frameStart = i * frameStep;
+            const frame = [];
 
-            frameStart = i * frameStep;
-            frame = [];
+            for (let j = 0; j < frameLength; j++) {
 
-            for (j = 0; j < frameLength; j++) {
-
-                frameIndex = j + frameStart;
+                const frameIndex = j + frameStart;
 
                 // Apply Hamming filter
-                filteredSample = sampleArray[frameIndex] * (0.54 - (0.46 * Math.cos(2.0 * Math.PI * j / (frameLength - 1.0))));
+                const filteredSample = sampleArray[frameIndex] * (0.54 - (0.46 * Math.cos(2.0 * Math.PI * j / (frameLength - 1.0))));
 
                 frame.push(filteredSample);
 
@@ -119,22 +119,22 @@ function drawSpectrogram(params, callback) {
 
         }
 
-        maxValue = 0;
-        minValue = 0;
+        let maxValue = 0;
+        let minValue = 0;
 
-        spectrumFrames = [];
+        let spectrumFrames = [];
 
-        for (m = 0; m < frames.length; m++) {
+        for (let m = 0; m < frames.length; m++) {
 
             // Apply FFT
-            let fft = new dsp.RFFT(nfft, sampleRate);
+            const fft = new dsp.RFFT(nfft, sampleRate);
             fft.forward(frames[m]);
 
-            spectrum = [];
+            const spectrum = [];
 
-            for (n = 0; n < fft.trans.length; n++) {
+            for (let n = 0; n < fft.trans.length; n++) {
 
-                if (fft.trans[n] != 0) {
+                if (fft.trans[n] !== 0) {
 
                     spectrum.push(Math.log(Math.abs(fft.trans[n])));
 
@@ -155,33 +155,33 @@ function drawSpectrogram(params, callback) {
         spectrumFrames = medianFilter(spectrumFrames);
 
         // Calculate range of filtered values to scale colours between
-        for (a = 0; a < spectrumFrames.length; a++) {
+        for (let a = 0; a < spectrumFrames.length; a++) {
 
             maxValue = Math.max(Math.max.apply(null, spectrumFrames[a]), maxValue);
             minValue = Math.min(Math.min.apply(null, spectrumFrames[a]), minValue);
 
         }
 
-        ctx = canvasElem.getContext("2d");
+        const ctx = canvasElem.getContext('2d');
 
         // Scale drawing context to fill canvas
-        specWidth = spectrumFrames.length;
-        specHeight = spectrumFrames[0].length / 2;
+        const specWidth = spectrumFrames.length;
+        const specHeight = spectrumFrames[0].length / 2;
         ctx.scale(canvasElem.width / specWidth, canvasElem.height / specHeight);
 
         // Create colourmap to map spectrum values to colours
-        colours = colormap({colormap: cmap, nshades: 255, format: 'hex'});
+        const colours = colormap({colormap: cmap, nshades: 255, format: 'hex'});
 
-        for (o = 0; o < spectrumFrames.length; o++) {
+        for (let o = 0; o < spectrumFrames.length; o++) {
 
             // Ignore half of spectrogram above Nyquist frequency as it is redundant a reflects values below
-            for (p = spectrumFrames[0].length / 2; p < spectrumFrames[0].length; p++) {
+            for (let p = spectrumFrames[0].length / 2; p < spectrumFrames[0].length; p++) {
 
                 // Scale values between 0 - 255 to match colour map
-                let scaledValue = Math.round(255 * scaleAcrossRange(spectrumFrames[o][p], maxValue, minValue));
+                const scaledValue = Math.round(255 * scaleAcrossRange(spectrumFrames[o][p], maxValue, minValue));
 
                 ctx.fillStyle = colours[scaledValue];
-                ctx.fillRect(o,p - spectrumFrames[0].length / 2,1,1);
+                ctx.fillRect(o, p - spectrumFrames[0].length / 2, 1, 1);
 
             }
 
